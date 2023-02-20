165afa8df12f3aaf1624e4bc07dceec22578fb35
GaneshVudugu <123009872+GaneshVudugu@users.noreply.github.com>
2023-02-20 21:47:29 +0530

    Add files via upload

---
+++ Back End Project 2/src/main/java/com/flightcontroller/Comformpayment.java
@@ -0,0 +1,83 @@
+package com.flightcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.flight.dao.Customer_Dao;
+import com.flight.models.Fare;
+import com.flight.models.Flight;
+import com.flight.models.Reservation;
+
+
+
+/**
+ * Servlet implementation class ConfirmPayment
+ */
+@WebServlet("/Comformpayment")
+
+public class Comformpayment extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@SuppressWarnings("unused")
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession(false);
+		String cardName = request.getParameter("cardname");
+		String cardNo = request.getParameter("cardno");
+		String date = request.getParameter("paymentdate");
+
+		if(session != null && (cardName != null && cardName.trim() != "") && (cardNo != null && cardNo.trim() != "" )
+				&& date != null) {
+
+			session.setAttribute("cardname", cardName);
+			session.setAttribute("cardno", cardNo);
+			session.setAttribute("paymentdate", date);
+
+			Flight flight = (Flight)session.getAttribute("flightobject");
+			Fare fare = (Fare)session.getAttribute("fareobject");
+			String travelDate = (String)session.getAttribute("traveldate");
+			int passengers = (int)session.getAttribute("passengers");
+			String day = (String)session.getAttribute("day");
+			int customerId = (int)session.getAttribute("customerId");
+			int bookingId = 0;
+
+			Reservation reservation = new Reservation();
+			Customer_Dao cust = new Customer_Dao();
+			double totalFare = cust.calculateFare(flight.getFlightNumber(), 
+					fare.getTravelClass() , passengers);
+
+			reservation.setFlightNumber(flight.getFlightNumber());
+			reservation.setTravelClass(fare.getTravelClass());
+			reservation.setTravelDate(cust.getDate(travelDate));
+			reservation.setPassengers(passengers);
+			reservation.setTotalFare(totalFare);
+			reservation.setCustomerId(customerId);
+
+			bookingId = cust.addReservation(reservation);
+
+			if(bookingId != 0) {
+				session.setAttribute("bookingId", bookingId);
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/bookingdetails.jsp");
+				rd.forward(request, response);
+			}
+		}else {
+			request.setAttribute("Error", "Error in processing payment please try again later");
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/payment.jsp");
+			rd.forward(request, response);
+		}
+
+	}
+
+
+}
---
+++ Back End Project 2/src/main/java/com/flightcontroller/Submit.java
@@ -0,0 +1,50 @@
+package com.flightcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.flight.dao.Customer_Dao;
+import com.flight.models.Fare;
+import com.flight.models.Flight;
+
+/**
+ * Servlet implementation class Submit
+ */
+@WebServlet("/Submit")
+public class Submit extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd;
+		Customer_Dao cust = new Customer_Dao();
+		
+		String travelClass = request.getParameter("travelclass");
+		HttpSession session = request.getSession(false);
+		Flight flight = (Flight)session.getAttribute("flightobject");
+		Fare fare = cust.getFareRecord(flight.getFlightNumber(), travelClass);
+		session.setAttribute("fareobject", fare);
+		
+		Integer customerId = (Integer)session.getAttribute("customerId");
+		
+		if(customerId == null) {
+		rd = getServletContext().getRequestDispatcher("/register.jsp");
+        rd.forward(request, response);
+		}else if(customerId != null) {
+			rd = getServletContext().getRequestDispatcher("/confirmbooking.jsp");
+	        rd.forward(request, response);
+		}
+	}
+
+
+}
